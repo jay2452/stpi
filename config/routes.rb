53004090e1befe943440f9128e809e766ebc8951
jay2452 <jay2452@live.com>
2016-03-04 17:36:34 +0530

    layouts changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :missions
   resources :services
   resources :faqs
   resources :sub_centers
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160302094718) do
+ActiveRecord::Schema.define(version: 20160304113118) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
@@ -62,6 +62,12 @@ ActiveRecord::Schema.define(version: 20160302094718) do
   end
 
   create_table "it_parks", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "missions", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
